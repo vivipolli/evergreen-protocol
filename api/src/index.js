acd96673b39b09f567a4fd4dc098beda649eed57
vivi <pollivivian@gmail.com>
2025-05-16 13:59:38 -0300

    fix cors in prod

--- api/src/index.js
+++ api/src/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://evergreen-protocol.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
